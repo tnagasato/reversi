759f74469844cb69d8b12482b0e170679ec55ae4
tomonori <tomonori.nagasato@gmail.com>
2021-09-20 23:08:57 +0900

    Django Secret Keyをgit管理外にする

--- reversi/settings.py
+++ reversi/settings.py
@@ -19,8 +19,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^6i8(+i$sc#p_+=h%#)+)zlo5^%jdc^fs5$7+bpl)zey5vj*m_'
+try:
+    from .local_settings import *
+except ImportError:
+    pass
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
